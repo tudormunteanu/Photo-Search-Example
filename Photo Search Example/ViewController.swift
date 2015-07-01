602d3a53d6e5e21d2c41f8d83d03efe54b710cfb
Shaun Chua <shon3005@gmail.com>
2015-07-01 15:29:49 +0800

    search bar update

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchInstagramByHashtag("dogs")
+    }
+    
+    func searchInstagramByHashtag(searchString:String) {
         let manager = AFHTTPRequestOperationManager()
-        
         manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=af21bd0936304303b7c7f37879c4e0ca",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
@@ -28,19 +32,34 @@ class ViewController: UIViewController {
                         }
                     }
                     self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    
+                    let imageWidth = self.view.frame.width
+                    self.scrollView.contentSize = CGSizeMake(imageWidth, imageWidth * CGFloat(dataArray.count))
+                    
                     for var i = 0; i < urlArray.count; i++ {
-                        let imageData = NSData(contentsOfURL: NSURL(string: urlArray[i])!)         //1
-                        if let imageDataUnwrapped = imageData {                                     //2
-                            let imageView = UIImageView(image: UIImage(data: imageDataUnwrapped))   //3
-                            imageView.frame = CGRectMake(0, 320 * CGFloat(i), 320, 320)               //4
-                            self.scrollView.addSubview(imageView)                                        //5
-                        }
+                        let imageView = UIImageView(frame: CGRectMake(0, imageWidth*CGFloat(i), imageWidth, imageWidth))
+                        imageView.setImageWithURL( NSURL(string: urlArray[i]))
+                        self.scrollView.addSubview(imageView)
+                        //let imageData = NSData(contentsOfURL: NSURL(string: urlArray[i])!)         //1
+                        //if let imageDataUnwrapped = imageData {                                     //2
+                            //let imageView = UIImageView(image: UIImage(data: imageDataUnwrapped))   //3
+                            //imageView.frame = CGRectMake(0, 320 * CGFloat(i), 320, 320)               //4
+                            //self.scrollView.addSubview(imageView)                                        //5
+                        //}
                     }
                 }
             },
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
         })
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
     }
 
     override func didReceiveMemoryWarning() {
